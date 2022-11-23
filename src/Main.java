548da0b1416f979b30d6713c8bddf3c7adcc3cdf
Tou4less <madserendi@gmail.com>
2022-11-23 16:39:07 +0300

    hello world, lesson 2

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+//   public static void main(String[] args) {
+//        Table table1 = new Table();
+//        table1.legs = 3;
+//        table1.wood = "pine";
+//        Object table2 = new Table();
+//
+//        System.out.println(table1);
+//        System.out.println(table2);
+//        short a = 100;
+//        int b = a;
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println("----------------");
+//
+//        int c = 30000;
+//        short d = (short) c;
+//        System.out.println(c);
+//        System.out.println(d);
+//        System.out.println("----------------");
+
+//        int g = 100_000;
+//        short f = (short) g;
+//        System.out.println("int - " + g);
+//        System.out.println("short - " + f);
+}
---
+++ src/Table.java
@@ -0,0 +1,12 @@
+public class Table {
+    int legs = 4;
+    String wood = "oak";
+
+    @Override
+    public String toString() {
+        return "Table{" +
+                "legs=" + legs +
+                ", wood='" + wood + '\'' +
+                '}';
+    }
+}
