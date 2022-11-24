60b68d73569994e7d870be169ec26a79df500323
Tou4less <madserendi@gmail.com>
2022-11-24 13:54:51 +0300

    gitignore, idea

---
+++ src/HelloUser.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class HelloUser {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("please input your name");
+        String a = in.nextLine();
+        System.out.println("Hello," +a);
+
+        System.out.println("please input your name");
+        String b = in.nextLine();
+        System.out.println("Hello," +b);
+
+        System.out.println("please input your name");
+        String c = in.nextLine();
+        System.out.println("Hello," +c);
+
+        System.out.println("please input your name");
+        String d = in.nextLine();
+        System.out.println("Hello," +d);
+
+        System.out.println("please input your name");
+        String e = in.nextLine();
+        System.out.println("Hello," +e);
+
+        System.out.println("please input your name");
+        String f = in.nextLine();
+        System.out.println("Hello," +f);
+
+        System.out.println("please input your name");
+        String g = in.nextLine();
+        System.out.println("Hello," +g);
+
+        System.out.println("please input your name");
+        String h = in.nextLine();
+        System.out.println("Hello," +h);
+
+        System.out.println("please input your name");
+        String i = in.nextLine();
+        System.out.println("Hello," +i);
+
+        System.out.println("please input your name");
+        String j = in.nextLine();
+        System.out.println("Hello," +j);
+    }
+}
